acc0371ee9a3de01c48e884ab0757f815b95ebad
trishantjain <trishantjain8888@gmail.com>
2024-02-03 01:41:27 +0530

    Added Designstack to About.js

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,6 +6,7 @@ import Techstack from "./Techstack";
 import Aboutcard from "./AboutCard";
 import laptopImg from "../../Assets/about.png";
 import Toolstack from "./Toolstack";
+import Designstack from "./Designstack"
 
 function About() {
   return (
@@ -44,6 +45,11 @@ function About() {
           <strong className="purple">Tools</strong> I use
         </h1>
         <Toolstack />
+
+        <h1 className="project-heading">
+          <strong className="purple">DESIGN</strong> Portfolio
+        </h1>
+        <Designstack />
 
         <Github />
       </Container>
