1b5d735ec404e1001f0bf9a70e952a167c805e0c
trishantjain <trishantjain8888@gmail.com>
2024-02-03 00:47:23 +0530

    Changed basic details

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,13 +8,10 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
+            Hi Everyone, I am <span className="purple">Trishant Jain </span>
+            from <span className="purple"> Faridabad, Haryana, India.</span>
             <br />
-            I am currently employed as a software developer at Juspay.
-            <br />
-            I have completed Integrated MSc (IMSc) in Maths and Computing at BIT
-            Mesra.
+            I am currently pursuing my graduation from J. C. University of Science & Technology, Faridabad.
             <br />
             <br />
             Apart from coding, some other activities that I love to do!
@@ -24,10 +21,10 @@ function AboutCard() {
               <ImPointRight /> Playing Games
             </li>
             <li className="about-activity">
-              <ImPointRight /> Writing Tech Blogs
+              <ImPointRight /> Graphic Designing
             </li>
             <li className="about-activity">
-              <ImPointRight /> Travelling
+              <ImPointRight /> Social Works
             </li>
           </ul>
 
