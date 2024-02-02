09c8f88996f174649f47e6690f53e276ff1dfb6b
trishantjain <trishantjain8888@gmail.com>
2024-02-03 01:43:13 +0530

    Removed unused code from Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -20,7 +20,7 @@ function Projects() {
         <p style={{ color: "white" }}>
           Here are a few projects I've worked on recently.
         </p>
-        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+        {/* <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={chatify}
@@ -87,7 +87,7 @@ function Projects() {
               // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
             />
           </Col>
-        </Row>
+        </Row> */}
       </Container>
     </Container>
   );
